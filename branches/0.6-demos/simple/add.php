8a447e4d0aae23d261c54da6dac0ab2771e7e6ed
brian.requinala <brian.requinala@247talk.net>
2012-07-03 15:22:41 +0000

    0.6-demos

---
+++ branches/0.6-demos/simple/add.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'config.php';
+
+require $C->add_dir.'/init.php';
+
+
+
+date_default_timezone_set('UTC');
+
+$G_errors = array();
+
+session_start();
+session_set_cookie_params(3600,$C->path,$C->domain,true);
+
+#add::canonicalize_path();
+add::current_controller()->page();
