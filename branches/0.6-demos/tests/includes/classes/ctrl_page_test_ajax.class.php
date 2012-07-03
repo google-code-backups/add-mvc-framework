a47f11b9371fefbe8f475b96044f9bd93b55d16b
brian.requinala <brian.requinala@247talk.net>
2012-07-03 22:38:06 +0000

    ctrl_page_test_ajax

--- branches/0.6-demos/tests/includes/classes/ctrl_page_test_ajax.class.php
+++ branches/0.6-demos/tests/includes/classes/ctrl_page_test_ajax.class.php
@@ -2,10 +2,12 @@
 
 CLASS ctrl_page_test_ajax EXTENDS ctrl_tpl_ajax {
 
-   public function execute() {
-      $query = 'testing testing lang'
+   public function process_data() {
+      $query = 'testing testing lang';
       $testing = array('testing' => 'arena', 'tester' => 'tesst');
       $this->assign('query', $query);
       $this->assign($testing);
    }
+   
+   
 }
