05b4585bcd746ede92d044ac97faf3a4d9d98182
brian.requinala@247talk.net <brian.requinala@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-03 17:01:20 +0000

    add_encryptor_test.php

---
+++ branches/0-6-tests/_T_add_encryptor_test.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'config.php';
+require_once "$C->add_dir/init.php";
+
+$string = 'bryan requinala';
+$key = generate_key();
+$encrypted_string = add_encryptor::string_encrypt($string,$key);
+$decrypted_string = add_encryptor::string_decrypt($string,$key);
+
+debug::var_dump($encrypted_string, $decrypted_string);
+
+
+
+?>
