afc5f49a2e6eb4727d150a726e988ef96eee9819
brian.requinala <brian.requinala@247talk.net>
2012-07-03 18:50:30 +0000

    testing of add_encryptor string encrypt and string decrypt

--- branches/0.6-demos/tests/add_encryptor.php
+++ branches/0.6-demos/tests/add_encryptor.php
@@ -1,15 +1,23 @@
 <?php
 
-require 'config.php';
+require '../config.php';
 require_once "$C->add_dir/init.php";
 
-$string = 'bryan requinala';
+$string = 'Bryan Requinala';
+
 $key = add_encryptor::generate_key();
+
 $encrypted_string = add_encryptor::string_encrypt($string,$key);
-$decrypted_string = add_encryptor::string_decrypt($string,$key);
+$decrypted_string = add_encryptor::string_decrypt($encrypted_string,$key);
 
-debug::var_dump($encrypted_string, $decrypted_string);
+debug::var_dump($encrypted_string);
+debug::var_dump($decrypted_string);
 
 
+$encrypted_string = add_encryptor::string_encrypt($string,$key);
+$decrypted_string = add_encryptor::string_decrypt($encrypted_string, 'asdasdasd');
+
+debug::var_dump($encrypted_string);
+debug::var_dump($decrypted_string);
 
 ?>
