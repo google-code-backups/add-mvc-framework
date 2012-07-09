fdbc5ec991db1a47986045b68c0b2ae114039190
jezieltabora <jezieltabora@247talk.net>
2012-07-09 19:09:38 +0000

    ctrl_page_sample_mailer.class.php

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -3,9 +3,18 @@ CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
 
    public function execute() {
    
-      $mailer = new ctrl_mailer_test();
+      //$mailer = new ctrl_mailer_test();
       
-      $mailer->Send();
+      //$mailer->Send();
+      
+      $to      = 'jezieltabora@247talk.net';
+      $subject = 'Sample Subject';
+      $message = 'Sample Message';
+      $headers = 'From: wjezieltabora@247talk.net' . "\r\n" .
+          'Reply-To: jezieltabora@247talk.net' . "\r\n" .
+          'X-Mailer: PHP/' . phpversion();
+
+      mail($to, $subject, $message, $headers);
       
       if (function_exists('mail'))
          echo "Exist";
