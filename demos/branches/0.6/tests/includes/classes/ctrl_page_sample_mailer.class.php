0d435d2af36faa3b411bd1dd0d67066e6506565f
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 21:03:23 +0000

    0.7: assert condition

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -3,7 +3,7 @@ CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
 
    public function execute() {
       error_reporting(E_ALL);
-      debug::var_dump(ini_set('sendmail_from', 'jezieltabora@247talk.net'));
+      var_dump(ini_set('sendmail_from', 'jezieltabora@247talk.net'));
       // Send
       $mail = mail('jezieltabora@247talk.net', 'My Subject', 'test body','From: jezieltabora@247talk.net');
 
