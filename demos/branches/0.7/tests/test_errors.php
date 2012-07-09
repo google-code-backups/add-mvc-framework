3c8a22f697499488a1f17d8e69c3a10e785a7267
brian.requinala <brian.requinala@247talk.net>
2012-07-09 19:08:33 +0000

    remove debug

--- demos/branches/0.7/tests/test_errors.php
+++ demos/branches/0.7/tests/test_errors.php
@@ -4,6 +4,5 @@ require 'add_configure.php';
 $divisor = 0;
 
 if ($divisor == 0) {
-debug::var_dump(    trigger_error("Cannot divide by zero", E_ERROR));
-
+    trigger_error("Cannot divide by zero", E_ERROR);
 }
