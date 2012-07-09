6eca2022a5239d83257b2afdeb6c70ab5e38cd56
jezieltabora@247talk.net <jezieltabora@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 21:29:07 +0000

    ctrl_mailer_test.class.php

--- demos/branches/0.6/tests/includes/classes/ctrl_mailer_test.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_mailer_test.class.php
@@ -12,8 +12,7 @@ CLASS ctrl_mailer_test EXTENDS ctrl_tpl_mailer {
 
    public $Subject           = 'Mailer Test';
 
-  public function __construct() {
-     parent::__construct(true);
+  public function process_data() {
      $this->AddAddress('jezieltabora@247talk.net');
      $this->Subject = "Test Only! This is the subject";
   }
