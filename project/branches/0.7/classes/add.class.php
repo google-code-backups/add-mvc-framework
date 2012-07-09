3de6cbc5ab330bfc373443bfee4e6b5315f99d4a
brian.requinala@247talk.net <brian.requinala@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 20:48:55 +0000

    change strict, notice color code

--- project/branches/0.7/classes/add.class.php
+++ project/branches/0.7/classes/add.class.php
@@ -259,9 +259,9 @@ CLASS add {
             E_ERROR        => 'background: #800517',
             E_USER_WARNING => 'background: #C11B17',
             E_USER_ERROR   => 'background: #C11B17',
-            E_USER_NOTICE  => 'background: #EB0000',
-            E_NOTICE       => 'background: #EB0000',
-            E_STRICT       => 'background: #EB0000',
+            E_USER_NOTICE  => 'background: #FF0080',
+            E_NOTICE       => 'background: #FF0080',
+            E_STRICT       => 'background: #FF0080',
       
       );
      
