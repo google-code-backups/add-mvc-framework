ae46e752ca22266383e24f03ecefd3c8b6d4775e
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 18:16:59 +0000

    project trunk

--- demos/branches/0.6/config.php
+++ demos/branches/0.6/config.php
@@ -1,6 +1,6 @@
 <?php
 $C = (object) array(
-      'add_dir'            => dirname(__FILE__).'/../0.6',
+      'add_dir'            => realpath('../../../../project/branches/0.6'),
       'super_domain'       => preg_replace('/^www\./','',$_SERVER['HTTP_HOST']),
       'sub_domain'         => 'www',
       'path'               => preg_replace('/\/[^\/]*?$/','/',$_SERVER['REQUEST_URI']),
