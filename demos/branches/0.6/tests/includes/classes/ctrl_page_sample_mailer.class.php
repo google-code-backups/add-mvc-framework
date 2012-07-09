fe306bbe6f24fceb2eac9fa9ce0175b9b01c332e
jezieltabora <jezieltabora@247talk.net>
2012-07-09 21:36:11 +0000

    ctrl_page_sample_mailer.class.php

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -1,18 +1,12 @@
 <?php
 CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
 
-   public function execute() {
+   public function process_data() {
 
       $mailer = new ctrl_mailer_test();
 
-      $mailer->IsSendmail();
-
-      $mailer->Send();
-
-      if (function_exists('mail'))
-         echo "Exist";
-      else
-         echo "Not Exist";
+      $mailer->execute();
+      
    }
 
 }
