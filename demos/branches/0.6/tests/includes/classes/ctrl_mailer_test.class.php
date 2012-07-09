2fb958db2b22635292f31aaef43542b4ffcd6322
albertdiones <albertdiones@gmail.com>
2012-07-09 18:56:54 +0000

    test

--- demos/branches/0.6/tests/includes/classes/ctrl_mailer_test.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_mailer_test.class.php
@@ -10,12 +10,7 @@ CLASS ctrl_mailer_test EXTENDS ctrl_tpl_mailer {
 
    public $FromName          = 'Testing';
 
-  public $Subject           = 'Mailer Test';
+   public $Subject           = 'Mailer Test';
 
-  public function __construct() {
-     parent::__construct();
-     $this->AddAddress('jezieltabora@247talk.net');
-     $this->Subject = "Test Only! This is the subject";
-  }
 
 }
