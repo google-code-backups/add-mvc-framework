6a72b277bde7c95bb6db36e929a61c6656b4e7b2
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 00:15:23 +0000

    default config

--- demos/branches/0.7/config.php
+++ demos/branches/0.7/config.php
@@ -1,18 +1,4 @@
 <?php
 $C = (object) array(
       'add_dir'            => '../../../../branches/0.7',
-      'super_domain'       => preg_replace('/^www\./','',$_SERVER['HTTP_HOST']),
-      'sub_domain'         => 'www',
-      'path'               => preg_replace('/\/[^\/]*?$/','/',$_SERVER['REQUEST_URI']),
-      'root_dir'           => realpath('./'),
-
-      /**
-       * Library init files
-       * @author albertdiones@gmail.com
-       */
-      'libs'            => (object) array(
-            'adodb'     => 'adodb/adodb.inc.php',
-            'smarty'    => 'smarty/Smarty.class.php',
-            'phpmailer' => 'phpmailer/class.phpmailer.php',
-         ),
    );
