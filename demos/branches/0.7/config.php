0b28b288f7d482fe30baf988a8c2a7d1c6a1ce92
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 00:45:34 +0000

    0.7: common_layout

--- demos/branches/0.7/config.php
+++ demos/branches/0.7/config.php
@@ -1,5 +1,5 @@
 <?php
 $C = (object) array(
-      'add_dir'            => '../../../../branches/0.7',
+      'add_dir'            => realpath('../../../../branches/0.7'),
       'environment_status' => 'development',
    );
