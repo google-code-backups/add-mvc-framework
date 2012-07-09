c6d918fab75691a7b4b948cf04c2718764014885
jezieltabora <jezieltabora@247talk.net>
2012-07-09 19:27:36 +0000

    ctrl_page_sample_mailer.class.php

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -7,7 +7,7 @@ CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
       
       //$mailer->Send();
       
-      $email_from = 'jeziel_reyuba@yahoo.com';
+      $email_from = 'jezieltabora@247talk.net';
       
       ini_set("sendmail_from", $email_from);
       
