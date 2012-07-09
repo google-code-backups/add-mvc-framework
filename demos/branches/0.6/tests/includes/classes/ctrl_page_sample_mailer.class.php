89a70042e56daaf0061ed7590b67d06cb90123bd
jezieltabora@247talk.net <jezieltabora@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 21:33:13 +0000

    ctrl_page_sample_mailer.class.php

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -1,25 +1,12 @@
 <?php
 CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
 
-   public function execute() {
-      error_reporting(E_ALL);
-      var_dump(ini_set('sendmail_from', 'jezieltabora@247talk.net'));
-      // Send
-      $mail = mail('jezieltabora@247talk.net', 'My Subject', 'test body','From: jezieltabora@247talk.net');
-
-      var_dump($mail);
-      var_dump(`sendmail`);
+   public function process_data() {
 
       $mailer = new ctrl_mailer_test();
 
-      $mailer->IsSendmail();
-
-      $mailer->Send();
-
-      if (function_exists('mail'))
-         echo "Exist";
-      else
-         echo "Not Exist";
+      $mailer->execute();
+      
    }
 
 }
