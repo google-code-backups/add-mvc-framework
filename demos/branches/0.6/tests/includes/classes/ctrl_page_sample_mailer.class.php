6097f1666f20dfb636016d3d696af63890164a59
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-09 20:25:20 +0000

    debug

--- demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
+++ demos/branches/0.6/tests/includes/classes/ctrl_page_sample_mailer.class.php
@@ -2,26 +2,19 @@
 CLASS ctrl_page_sample_mailer EXTENDS ctrl_tpl_page {
 
    public function execute() {
-   
-      $email_from = 'jezieltabora@247talk.net';
-      
-      ini_set("sendmail_from", $email_from);
-      
-      // The message
-      $message = "Line 1\nLine 2\nLine 3";
 
       // Send
-      $mail = mail('jezieltabora@247talk.net', 'My Subject', $message);
-      
+      $mail = mail('jezieltabora@247talk.net', 'My Subject', 'test body');
+
       var_dump($mail);
-      
+
       $mailer = new ctrl_mailer_test();
-      
+
       $mailer->Send();
-      
+
       if (function_exists('mail'))
          echo "Exist";
-      else 
+      else
          echo "Not Exist";
    }
 
