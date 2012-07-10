5a1813b5b1eebdafd40191bfe142b07146dcb79f
albertdiones <albertdiones@gmail.com>
2012-07-10 21:36:54 +0000

    error message tpls

--- project/branches/0.7/classes/add.class.php
+++ project/branches/0.7/classes/add.class.php
@@ -220,18 +220,18 @@ CLASS add {
       global $G_errors;
 
       static $error_code_strings = array(
-            E_ERROR => 'E_ERROR',
-            E_WARNING => 'E_WARNING',
-            E_PARSE => 'E_PARSE',
-            E_NOTICE => 'E_NOTICE',
-            E_STRICT => 'E_STRICT',
-            E_CORE_ERROR => 'E_CORE_ERROR',
-            E_CORE_WARNING => 'E_CORE_WARNING',
-            E_COMPILE_ERROR => 'E_COMPILE_ERROR',
+            E_ERROR           => 'E_ERROR',
+            E_WARNING         => 'E_WARNING',
+            E_PARSE           => 'E_PARSE',
+            E_NOTICE          => 'E_NOTICE',
+            E_STRICT          => 'E_STRICT',
+            E_CORE_ERROR      => 'E_CORE_ERROR',
+            E_CORE_WARNING    => 'E_CORE_WARNING',
+            E_COMPILE_ERROR   => 'E_COMPILE_ERROR',
             E_COMPILE_WARNING => 'E_COMPILE_WARNING',
-            E_USER_ERROR => 'E_USER_ERROR',
-            E_USER_WARNING => 'E_USER_WARNING',
-            E_USER_NOTICE => 'E_USER_NOTICE'
+            E_USER_ERROR      => 'E_USER_ERROR',
+            E_USER_WARNING    => 'E_USER_WARNING',
+            E_USER_NOTICE     => 'E_USER_NOTICE'
          );
 
       static $error_code_readable_strings = array(
