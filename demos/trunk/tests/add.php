ffb188d3b4b8860003dd7c84c606927ba42ddf6b
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-10 20:23:22 +0000

    Demo 0.6

--- demos/trunk/tests/add.php
+++ demos/trunk/tests/add.php
@@ -4,14 +4,10 @@ require '../config.php';
 
 require $C->add_dir.'/init.php';
 
-
-
 date_default_timezone_set('UTC');
-
-$G_errors = array();
 
 session_start();
 session_set_cookie_params(3600,$C->path,$C->domain,true);
 
 #add::canonicalize_path();
-add::current_controller()->page();
+add::current_controller()->execute();
---
+++ demos/trunk/tests/includes/classes/ctrl_mailer_test.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ctrl_page_test
+ *
+ */
+CLASS ctrl_mailer_test EXTENDS ctrl_tpl_mailer {
+
+   public $From              = 'jezieltabora@247talk.net';
+
+   public $FromName          = 'Testing';
+
+   public $Subject           = 'Mailer Test';
+
+  public function process_data() {
+     $this->AddAddress('jezieltabora@247talk.net');
+     $this->Subject = "Test Only! This is the subject";
+  }
+
+}
