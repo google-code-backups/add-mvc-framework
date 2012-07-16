7236bb4072c45895bf7152ed19f708a0cf522468
albertdiones <albertdiones@gmail.com>
2012-07-16 20:02:02 +0000

    test syntax : debug

--- demos/branches/0.7/tests/test_e_syntax.php
+++ demos/branches/0.7/tests/test_e_syntax.php
@@ -3,7 +3,7 @@
 require '../config.php';
 require_once "$C->add_dir/init.php";
 
-add::php_check_syntax('test_e_syntax2.php');
+add::php_check_syntax(realpath('test_e_syntax2.php'));
 
 ?>
 Syntax checked
