63af0d51d610fd6028bfae3166fdfb5b3cfca6aa
jezieltabora <jezieltabora@247talk.net>
2012-07-16 22:47:20 +0000

    test_exceptions.php

--- demos/branches/0.7/tests/test_exceptions.php
+++ demos/branches/0.7/tests/test_exceptions.php
@@ -5,5 +5,5 @@ require_once "$C->add_dir/init.php";
 $exceptions = array('e_developer','e_syntax','e_hack','e_spam','e_system');
 $rand_excemption = $exceptions[array_rand($exceptions)];
 
-throw new echo $rand_excemption('ERROR!');
+throw new $rand_excemption('ERROR! '.$rand_excemption);
 ?>
