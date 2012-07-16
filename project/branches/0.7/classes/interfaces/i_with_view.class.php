674cef95cbbd941c05b41763c4c562cfea89e63d
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-16 18:57:35 +0000

    Interface for classes with view

--- project/branches/0.7/classes/interfaces/i_with_view.class.php
+++ project/branches/0.7/classes/interfaces/i_with_view.class.php
@@ -9,25 +9,24 @@ INTERFACE i_with_view {
 
    /**
     * The view filepath of $this controller
-    * @since ADD MVC 0.3
+    * @since ADD MVC 0.7
     */
    public static function view_filepath();
    /**
     * The view base file name of $this controller
-    * @since ADD MVC 0.3
-    * @version 0.1
+    * @since ADD MVC 0.7
     */
    public static function view_basename();
 
    /**
     * The controller's basename
-    * @since ADD MVC 0.3
+    * @since ADD MVC 0.7
     */
    public static function basename();
 
    /**
     * The Smarty view object of $this controller
-    * @since ADD MVC 0.3
+    * @since ADD MVC 0.7
     */
    public function view();
 
