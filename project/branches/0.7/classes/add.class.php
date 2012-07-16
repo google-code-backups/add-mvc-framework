df91943476efeee637bfdb97e0e51668e4c73626
albertdiones <albertdiones@gmail.com>
2012-07-16 21:09:11 +0000

    code on error

--- project/branches/0.7/classes/add.class.php
+++ project/branches/0.7/classes/add.class.php
@@ -359,7 +359,7 @@ CLASS add {
     * @since ADD MVC 0.7
     */
    public static function php_check_syntax($filepath) {
-      $cmd_line = PHP_BINARY.' -n -l '.escapeshellarg($filepath);
+      $cmd_line = 'php -n -l '.escapeshellarg($filepath);
       $output = shell_exec($cmd_line);
 
       if ($output) {
