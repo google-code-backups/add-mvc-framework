c68697b825502a851595be6bdc492d16a55426b3
albertdiones <albertdiones@gmail.com>
2012-07-16 18:53:40 +0000

    EXTENDS i_with_view

--- project/branches/0.7/classes/interfaces/i_ctrl_with_view.class.php
+++ project/branches/0.7/classes/interfaces/i_ctrl_with_view.class.php
@@ -6,32 +6,7 @@
  *
  * @package ADD MVC\Controllers
  * @since ADD MVC 0.3
- * @version 0.0
+ * @version 1.0
  */
-INTERFACE i_ctrl_with_view {
-
-   /**
-    * The view filepath of $this controller
-    * @since ADD MVC 0.3
-    */
-   public static function view_filepath();
-   /**
-    * The view base file name of $this controller
-    * @since ADD MVC 0.3
-    * @version 0.1
-    */
-   public static function view_basename();
-
-   /**
-    * The controller's basename
-    * @since ADD MVC 0.3
-    */
-   public static function basename();
-
-   /**
-    * The Smarty view object of $this controller
-    * @since ADD MVC 0.3
-    */
-   public function view();
-
+INTERFACE i_ctrl_with_view EXTENDS i_with_view {
 }
