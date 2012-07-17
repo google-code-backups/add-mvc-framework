3c6b3bb38e06e45839a2331ce61f7cac973d200d
jezieltabora@247talk.net <jezieltabora@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-17 16:02:00 +0000

    add.php

--- demos/branches/0.7/tests/add.php
+++ demos/branches/0.7/tests/add.php
@@ -1,6 +1,5 @@
 <?php
 
 require 'add_configure.php';
-e_add::$email_addresses = 'albertdiones@247talk.net, jezieltabora@247talk.net, brian.requinala@247talk.net';
 #add::ca nonicalize_path();
 add::current_controller()->execute();
