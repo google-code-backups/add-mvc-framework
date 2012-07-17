4d8789460db05a09ae75b68d9ac963ef495fd51d
jezieltabora <jezieltabora@247talk.net>
2012-07-17 16:10:18 +0000

    test_exceptions.php

--- demos/branches/0.7/tests/test_exceptions.php
+++ demos/branches/0.7/tests/test_exceptions.php
@@ -6,7 +6,7 @@ add::config()->environment_status = 'live';
 
 $email = $_GET['email'];
 if ($email) {
-   e_add::$email_addresses = 'albertdiones@247talk.net, jezieltabora@247talk.net, brian.requinala@247talk.net';
+   e_add::$email_addresses = $email;
    $exceptions = array('e_developer','e_syntax','e_hack','e_spam','e_system');
    $rand_excemption = $exceptions[array_rand($exceptions)];
    throw new $rand_excemption('ERROR! '.$rand_excemption);
