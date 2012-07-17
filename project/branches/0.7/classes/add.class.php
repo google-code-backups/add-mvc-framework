93a1a6a8cbeb6ce74fcafb7a8c363e85135f164b
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-17 22:51:49 +0000

    error backtrace

--- project/branches/0.7/classes/add.class.php
+++ project/branches/0.7/classes/add.class.php
@@ -337,7 +337,11 @@ CLASS add {
 
                $code_on_error_padding = 3;
 
-               for ($code_on_error_x = $file - $code_on_error_padding; $code_on_error_x <= ($error['line'] + $code_on_error_padding); $code_on_error_x++) {
+               $code_on_error_start = max($error['line'] - 3,1)
+
+               $smarty->assign('code_on_error_start', $code_on_error_start);
+
+               for ($code_on_error_x = $code_on_error_start; $code_on_error_x <= ($error['line'] + $code_on_error_padding); $code_on_error_x++) {
                   $code_on_error .= $file_codes[$code_on_error_x+1];
                }
 
