102d3067a777faa9cbeb06843848a63ca061c908
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-07-20 19:28:16 +0000

    add demo

--- demos/trunk/config.php
+++ demos/trunk/config.php
@@ -1,5 +1,5 @@
 <?php
 $C = (object) array(
-      'add_dir'            => realpath('../../../../project/trunk'),
+      'add_dir'            => realpath('../../../project/trunk'),
       'environment_status' => 'development',
    );
