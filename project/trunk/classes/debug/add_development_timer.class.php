355582144c822cf45feedc5b97b75b100d5f2d7a
albertdiones <albertdiones@gmail.com>
2012-07-23 17:23:07 +0000

    print all laps

--- project/trunk/classes/debug/add_development_timer.class.php
+++ project/trunk/classes/debug/add_development_timer.class.php
@@ -18,9 +18,9 @@ CLASS add_development_timer EXTENDS add_debug_timer {
    * @since ADD MVC 0.0
    * @version 0.1
    */
-   public function print_lap($label=NULL) {
+   public function print_lap_difference($label=NULL) {
       echo "<div style='text-align:center; padding:10px 20px; margin: 10px 20px;'>";
-      parent::print_lap($label);
+      parent::print_lap_difference($label);
       echo "</div>";
    }
 }
