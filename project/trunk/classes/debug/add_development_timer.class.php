3f0153ace635977b83f1adea5df91313e8310972
albertdiones <albertdiones@gmail.com>
2012-07-23 16:40:59 +0000

    When development, record the time spent on script execution : debug

--- project/trunk/classes/debug/add_development_timer.class.php
+++ project/trunk/classes/debug/add_development_timer.class.php
@@ -9,4 +9,18 @@ CLASS add_development_timer EXTENDS add_debug_timer {
    public static function current_user_allowed() {
       return !add::is_live();
    }
+
+  /**
+   * Add a lap with $label and print the details
+   * @param string $label the description of the line where it is placed (eg. "after mysql query" )
+   * @author albertdiones@gmail.com
+   *
+   * @since ADD MVC 0.0
+   * @version 0.1
+   */
+   public function print_lap($label=NULL) {
+      echo "<div style='text-align:center; padding:10px 20px; margin: 10px 20px;'>";
+      parent::print_lap($label);
+      echo "</div>";
+   }
 }
