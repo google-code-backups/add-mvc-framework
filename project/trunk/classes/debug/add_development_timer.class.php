8ccf431ce11bd110beb7942b6c97bb09eddeba54
albertdiones <albertdiones@gmail.com>
2012-08-08 23:50:29 +0000

    [0.7.4] debug::print_data

--- project/trunk/classes/debug/add_development_timer.class.php
+++ project/trunk/classes/debug/add_development_timer.class.php
@@ -10,14 +10,4 @@ CLASS add_development_timer EXTENDS add_debug_timer {
       return !add::is_live();
    }
 
-  /**
-   * print_lap_difference between two laps, additional style for development timer
-   *
-   * @since ADD MVC 0.7.2
-   */
-   public function print_lap_difference($lap1, $lap2) {
-      echo "<div style='text-align:center; padding:10px 20px; margin: 10px 20px;'>";
-      parent::print_lap_difference($lap1,$lap2);
-      echo "</div>";
-   }
 }
