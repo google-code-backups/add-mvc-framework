cc577614c629d281f28a0d3757f83727ba6f036b
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-08-08 23:18:22 +0000

    [0.7.4] print environment status when not live

--- project/trunk/classes/add.class.php
+++ project/trunk/classes/add.class.php
@@ -396,6 +396,8 @@ CLASS add {
          global $add_mvc_root_timer;
 
          if (isset($add_mvc_root_timer) && $add_mvc_root_timer instanceof add_debug_timer) {
+            if (!add::is_live())
+               debug::print_config('environment_status');
             $add_mvc_root_timer->lap("Shutdown");
             $add_mvc_root_timer->print_all_laps();
          }
