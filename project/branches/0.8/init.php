0cdaa4dca7afa05481f98839e6d2d49a5c177e95
albertdiones@gmail.com <albertdiones@gmail.com@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-08-16 23:32:07 +0000

    iterate through class directory variable for autoloading

--- project/branches/0.8/init.php
+++ project/branches/0.8/init.php
@@ -35,7 +35,7 @@ register_shutdown_function('add::handle_shutdown');
 
 $C->incs_dir            = $C->root_dir.'/includes';
 
-$C->classes_dirs         = array_merge(array( $C->add_dir.'/classes', $C->incs_dir.'/classes' ), $C->classes_dirs);
+$C->classes_dirs        = array_merge( array( $C->add_dir.'/classes', $C->incs_dir.'/classes' ), is_array($C->classes_dirs) ? $C->classes_dirs : array());
 
 $C->configs_dir         = $C->incs_dir.'/configs';
 $C->views_dir           = $C->incs_dir.'/views';
