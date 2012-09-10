6555e399383da545ddfaf20a561bb8d3d68aaa02
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-09-10 16:17:36 +0000

    sample aux file

---
+++ demos/branches/0.8/simple/includes/classes/ctrl_page_aux.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * aux controller
+ *
+ * @since ADD MVC Demo 0.8
+ */
+CLASS ctrl_page_aux EXTENDS ctrl_tpl_aux {
+
+   /**
+    * Test echo
+    *
+    * @since ADD MVC 0.8
+    */
+   public function process_data() {
+      echo "Hello World\r\n";
+      echo "This is a test";
+   }
+
+   /**
+    * Can run this script?
+    *
+    * @since since
+    */
+   public function can_run() {
+      return true;#add::is_developer();
+   }
+}
