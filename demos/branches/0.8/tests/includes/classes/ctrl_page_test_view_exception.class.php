787719aa41f0bfaa5d1e90d010e9d1b7de57e5ee
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-09-17 17:33:39 +0000

    bug fix: prevent displaying exception view if headers are already sent

--- demos/branches/0.8/tests/includes/classes/ctrl_page_test_view_exception.class.php
+++ demos/branches/0.8/tests/includes/classes/ctrl_page_test_view_exception.class.php
@@ -1,7 +1,7 @@
 <?php
 CLASS ctrl_page_test_view_exception EXTENDS ctrl_tpl_page {
 
-   public static throw_exception() {
+   public static function throw_exception() {
       throw new e_developer("Test!");
    }
 }
