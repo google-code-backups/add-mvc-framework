084410b31d3fa5bdc01190366ce24b257b74b775
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-09-19 23:01:13 +0000

    Re-branched version 0.9

---
+++ project/branches/0.9/classes/interfaces/i_ctrl.class.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * i_ctrl controller interface
+ *
+ * @since ADD MVC 0.6
+ */
+INTERFACE i_ctrl {
+
+   /**
+    * execute() the execute function
+    *
+    * @since ADD MVC 0.6
+    */
+   public function execute();
+
+   /**
+    * print_response() - print the response according to data
+    *
+    * @since ADD MVC 0.6
+    */
+   public function print_response($data);
+
+
+   /**
+    * process_data() - process the data
+    *
+    * @since ADD MVC 0.6
+    */
+   public function process_data();
+
+   /**
+    * assign data
+    *
+    * @since ADD MVC 0.6
+    */
+   public function assign(/* Polymorphic */);
+
+
+   /**
+    * sets the content_type or get the current one
+    *
+    * @param string $new_content_type
+    *
+    * @since ADD MVC 0.8
+    */
+   public function content_type($new_content_type = null);
+}
