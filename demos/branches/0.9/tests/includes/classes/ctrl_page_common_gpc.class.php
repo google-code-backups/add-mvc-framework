d3dcac16aacd799c249e219059e592f55d83d9e3
brian.requinala <brian.requinala@247talk.net>
2012-09-24 20:58:32 +0000

    common_gpc.class.php

---
+++ demos/branches/0.9/tests/includes/classes/ctrl_page_common_gpc.class.php
@@ -0,0 +1,37 @@
+<?php
+
+CLASS ctrl_page_common_gpc EXTENDS ctrl_tpl_page {
+
+   protected $common_gpc = array(
+      '_POST' => array ('first_name','last_name'),
+   );
+
+   protected $mode_gpc_my_age = array(
+      '_POST' => array ('age', 'birthday');
+   );
+
+   protected $mode_gpc_my_address = array(
+      '_POST' => array ('street_number', 'city', 'state');
+   );
+
+
+   public function process_mode_my_age($gpc) {
+      extract($gpc);
+      $this->full_name($first_name, $last_name);
+      $this->assign('age', $age);
+      $this->assign('bday', $birthday);
+   }
+
+   public function process_mode_my_address($gpc) {
+      $this->full_name($first_name, $last_name);
+      $this->assign('street_no', $street_number);
+      $this->assign('city', $city);
+      $this->assign('state', $state);
+   }
+
+   public function full_name($first, $last) {
+     $full_name = $first.' '.$last;
+     $this->assign('name', $full_name);
+   }
+
+}
