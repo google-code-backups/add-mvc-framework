0f2c7f7c6c594d4a7ba17fec59029105e9b0181d
brian.requinala@247talk.net <brian.requinala@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-09-25 15:49:13 +0000

    test ajax

--- demos/branches/0.9/tests/includes/classes/ctrl_page_test_ajax.class.php
+++ demos/branches/0.9/tests/includes/classes/ctrl_page_test_ajax.class.php
@@ -2,12 +2,12 @@
 
 CLASS ctrl_page_test_ajax EXTENDS ctrl_tpl_ajax {
 
-   public function process_data() {
+   public function process_data( $common_gpc ) {
       $query = 'testing testing lang';
       $testing = array('testing' => 'arena', 'tester' => 'tesst');
       $this->assign('query', $query);
       $this->assign($testing);
    }
-   
-   
+
+
 }
