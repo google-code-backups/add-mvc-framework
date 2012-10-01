13a3d97f5393c3759f62d883890bdb787a8761a3
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-10-01 21:40:00 +0000

    add_layout and add_include should not have files .tpl

--- project/branches/0.10/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
+++ project/branches/0.10/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
@@ -14,7 +14,8 @@ CLASS Smarty_Internal_Compile_Add_Layout EXTENDS Smarty_Internal_Compile_Extends
     */
    public function getAttributes() {
       $result = call_user_func_array('parent::'.__FUNCTION__,func_get_args());
-      $result['file'] = preg_replace('/^\\\'|\"/','$0layouts/',$result['file']);
+      $result['file'] = preg_replace('/^(\\\'|\")/','$0layouts/',$result['file']);
+      $result['file'] = preg_replace('/(\\\'|\")$/','.tpl$0',$result['file']);
       return $result;
    }
 }
