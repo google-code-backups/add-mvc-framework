55ce40d28744a8cc6c5679e9b5a171b01fdaf261
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-10-01 21:52:54 +0000

    bug fix: better error message on template not found on include

--- project/branches/0.10/libs/smarty/sysplugins/smarty_internal_compile_add_include.php
+++ project/branches/0.10/libs/smarty/sysplugins/smarty_internal_compile_add_include.php
@@ -16,6 +16,9 @@ CLASS Smarty_Internal_Compile_Add_Include EXTENDS Smarty_Internal_Compile_Includ
       $result = call_user_func_array('parent::'.__FUNCTION__,func_get_args());
       $result['file'] = preg_replace('/^(\\\'|\")/','$0includes/',$result['file']);
       $result['file'] = preg_replace('/(\\\'|\")$/','.tpl$0',$result['file']);
+
+      e_developer::assert($this->smarty->TemplateExists($result['file']),"$result[file] Does not exist!");
+
       return $result;
    }
 }
