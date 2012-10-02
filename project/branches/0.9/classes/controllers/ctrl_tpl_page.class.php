9f6432b4c2d861bad3e674eda476c8824309de1e
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-10-02 19:29:43 +0000

    removed usage of i_ctrl

--- project/branches/0.9/classes/controllers/ctrl_tpl_page.class.php
+++ project/branches/0.9/classes/controllers/ctrl_tpl_page.class.php
@@ -7,8 +7,11 @@
  * @package ADD MVC\Controllers
  * @since ADD MVC 0.0
  * @version 1.0
+ *
+ * @todo ADD MVC version 1.0: implement i_ctrl (version 0.9) for this
+ *
  */
-ABSTRACT CLASS ctrl_tpl_page IMPLEMENTS i_ctrl_0_9, i_ctrl_with_view {
+ABSTRACT CLASS ctrl_tpl_page IMPLEMENTS i_ctrl_with_view {
 
    /**
     * The mode of the process
