8f26e9802f698332d5c0c24ea1e7416473649a07
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-10-18 16:02:20 +0000

    sub_mode support: abstract ctrl

--- project/trunk/classes/controllers/ctrl_tpl_page.class.php
+++ project/trunk/classes/controllers/ctrl_tpl_page.class.php
@@ -11,7 +11,7 @@
  * @todo ADD MVC version 1.0: implement i_ctrl (version 0.9) for this
  *
  */
-ABSTRACT CLASS ctrl_tpl_page IMPLEMENTS i_ctrl_with_view {
+ABSTRACT CLASS ctrl_tpl_page EXTENDS abstract_ctrl IMPLEMENTS i_ctrl_with_view {
 
    /**
     * The mode of the process
@@ -129,26 +129,6 @@ ABSTRACT CLASS ctrl_tpl_page IMPLEMENTS i_ctrl_with_view {
    }
 
 
-   /**
-    * Set mode function
-    *
-    * @since ADD MVC 0.10
-    */
-   public function set_mode() {
-      if (isset($_REQUEST['mode'])) {
-         if (preg_match('/^\w+$/',$_REQUEST['mode'])) {
-            $this->mode = $_REQUEST['mode'];
-            if (preg_match('/^\w+$/',$_REQUEST['sub_mode'])) {
-               $this->sub_mode = $_REQUEST['sub_mode'];
-            }
-         }
-         else {
-         }
-      }
-
-      if (!isset($this->mode))
-         $this->mode = 'default';
-   }
 
    /**
     * The pre-display process of the controller
@@ -173,49 +153,6 @@ ABSTRACT CLASS ctrl_tpl_page IMPLEMENTS i_ctrl_with_view {
    }
 
 
-   /**
-    * process_mode function
-    * Processes any GPC requests
-    *
-    * @param array $gpc
-    * @since ADD MVC 0.1
-    *
-    * @version 1.0
-    */
-   public function process_mode( $common_gpc = array() ) {
-      $mode = $this->mode;
-
-      $method_name = "process_mode_$mode";
-
-      if (method_exists($this,$method_name)) {
-
-         $gpc_key_var = "mode_gpc_$mode";
-
-         $compact_array = array();
-
-         if (isset($this->$gpc_key_var)) {
-            $compact_array = $this->recursive_compact( $this->$gpc_key_var );
-         }
-         else if ($mode != 'default') {
-            throw new e_developer(get_called_class()."->$gpc_key_var not declared");
-         }
-
-         $merge_compact_array = array_merge($common_gpc, $compact_array);
-
-         $this->assign($merge_compact_array);
-         $this->assign('mode',$mode);
-         $this->assign('sub_mode',$this->sub_mode);
-
-         return $this->$method_name($merge_compact_array);
-
-      }
-      else if ($mode == 'default') {
-         $this->assign($common_gpc);
-         $this->assign('mode','default');
-      }
-
-      return false;
-   }
 
    /**
     * recursive compact($array_keys)
