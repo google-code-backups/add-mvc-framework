ce067e946c589ac624ea35966bc67441868b5a1a
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-11-01 16:31:52 +0000

    Exception fixes

--- demos/trunk/tests/test_exceptions.php
+++ demos/trunk/tests/test_exceptions.php
@@ -2,17 +2,30 @@
 require '../config.php';
 require_once "$C->add_dir/init.php";
 
-add::config()->environment_status = 'live';
 
-$email = $_GET['email'];
-if ($email) {
+
+$exceptions = array('e_developer','e_syntax','e_hack','e_spam','e_system');
+$exception = (string) $_GET['exception'];
+$email = (string) $_GET['email'];
+if ($exception) {
+   if ($_REQUEST['is_live'])
+      add::config()->environment_status = 'live';
+   add::content_type($_REQUEST['content_type']);
    e_add::$email_addresses = $email;
-   $exceptions = array('e_developer','e_syntax','e_hack','e_spam','e_system');
-   $rand_excemption = $exceptions[array_rand($exceptions)];
-   throw new $rand_excemption('ERROR! '.$rand_excemption);
+   throw new $exception("Test Error Message");
 }
 ?>
 <form method="GET">
-   <input type="text" name="email" />
+   <select name="exception">
+      <option><?php echo implode("</option><option>",$exceptions) ?></option>
+   </select>
+   <label>Email<input type="text" name="email" /></label>
+   <label><input name="is_live" type="checkbox" />Live</label>
+   <label>
+      Content Type<select name="content_type">
+         <option>text/html</option>
+         <option>text/plain</option>
+      </select>
+   </label>
    <input type="submit" />
 </form>
--- project/trunk/views/exceptions/e_database.tpl
+++ project/trunk/views/exceptions/e_database.tpl
@@ -3,12 +3,12 @@
 {block name='main'}
 {if add::content_type() == 'text/plain'}
 Database Error
-   
+
    {$user_message|default: 'A database error occured. Our developers has been notified about this and we will fix it as soon as we can.' }
 {else}
 <h1>Database Error</h1>
    {if $user_message}
-      {$user_message}
+      <p>{$user_message}</p>
    {else}
       <p>A database error occured. Our developers has been notified about this and we will fix it as soon as we can.</p>
    {/if}
