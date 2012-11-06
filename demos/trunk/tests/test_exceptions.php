caf0d7662ab45f58a29062a82e481d74e3f630b9
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2012-11-06 20:50:16 +0000

    test exceptions improvements

--- demos/trunk/tests/test_exceptions.php
+++ demos/trunk/tests/test_exceptions.php
@@ -9,10 +9,12 @@ $exception = (string) $_GET['exception'];
 $email = (string) $_GET['email'];
 if ($exception) {
    if ($_REQUEST['is_live'])
-      add::config()->environment_status = 'live';
+      add::environment_status( 'live' );
    add::content_type($_REQUEST['content_type']);
    e_add::$email_addresses = $email;
-   throw new $exception("Test Error Message");
+   $e = new $exception("Test Error Message");
+   debug::var_dump($e->mail_body());
+   #throw new $exception("Test Error Message");
 }
 ?>
 <form method="GET">
