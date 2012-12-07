3d9ae4f0489a80637a2cda41b20841bf6b32f4c1
brian.requinala <brian.requinala@247talk.net>
2012-12-07 19:55:27 +0000

    add url.functions.php

---
+++ project/trunk/functions/url.functions.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Returns the complete url according to $base
+ *
+ * @since ADD MVC 0.5
+ */
+function build_url($base,$url) {
+  $base_parts=url_parts($base);
+
+  if ($url[0]==='/') {
+    return rtrim($base_parts['protocol_domain'],'/').$url;
+  }
+  if ($url[0]==='?') {
+    if (!$base_parts['pathname'])
+      $base_parts['pathname']='/';
+    return $base_parts['protocol_domain'].$base_parts['pathname'].$url;
+  }
+  if ($url[0]==='#') {
+
+  }
+  if (preg_match('/^https?\:\/+/',$url)) {
+    return $url;
+  }
+
+  return rtrim($base_parts['protocol_domain'],"/").$base_parts['path'].$url;
+}
+
+/**
+ * Returns the URL parts of the url
+ *
+ * @since ADD MVC 0.5
+ */
+function url_parts($url) {
+  if (!preg_match('/^(?P<protocol_domain>(?P<protocol>https?\:\/+)(?P<domain>([^\/\W]|[\.\-])+))(?P<request_uri>(?P<pathname>(?P<path>\/(.+\/)?)?(?P<file>[^\?\#]+?)?)?(?P<query_string>\?[^\#]*)?)(\#(?P<hash>.*))?$/',$url,$url_parts)) {
+    echo debug_backtrace();
+    throw new Exception("Invalid url: $url");
+  }
+  return $url_parts;
+}
+?>
