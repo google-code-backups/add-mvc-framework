19b358452bf8ece270217e1f9baa5e3ace04f438
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-01-15 20:49:23 +0000

    bug fix: date.functions.php, geo.functions.php and hash.functions.php should not be released

--- project/trunk/functions/hash.functions.php
+++ project/trunk/functions/hash.functions.php
@@ -2,6 +2,8 @@
 /**
  * Hash functions
  *
+ * This should be deprecated along with date.functions.php but this can be useful for others anyway so...
+ *
  * @author albertdiones@gmail.com
  *
  * @package ADD MVC\Functions
