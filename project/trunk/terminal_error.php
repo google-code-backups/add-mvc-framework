a2a84b9304f2c69ba1e6ec3f9824620167709738
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-01 22:50:03 +0000

    Fix Issue: http://code.google.com/p/add-mvc-framework/issues/detail?id=24
    Terminal Error Page

---
+++ project/trunk/terminal_error.php
@@ -0,0 +1,58 @@
+<?php
+namespace add\terminal_error;
+#var_dump($GLOBALS);
+
+$error_message = error_message;
+
+$error_message = preg_replace('/(?<= )(\S{30,})(?= )/','<br />\1<br />',$error_message);
+
+?>
+<html>
+   <head>
+      <style>
+         body {
+            background:#eee;
+            font:12px verdana;
+         }
+         section.page {
+            color:#000;
+            background:#fff;
+            border:1px solid #000;
+            border-radius:5px;
+            margin:50px auto;
+            width:600px;
+         }
+         header {
+            padding:5px 10px;
+            background:#333;
+            color:#fff;
+            font-size:8px;
+            font-weight:bold;
+            border-radius:5px 5px 0 0;
+         }
+         h1 {
+            padding:5px 10px;
+            font-weight:normal;
+            font:12px verdana;
+            text-align:center;
+         }
+         footer {
+            padding:5px 10px;
+            background:#333;
+            color:#fff;
+            font-weight:bold;
+            font-size:8px;
+            border-radius:0 0 5px 5px;
+         }
+      </style>
+   </head>
+   <body>
+      <section class="page">
+         <header><?php echo error_header ?></header>
+         <article>
+            <h1><?php echo $error_message ?></h1>
+         </article>
+         <footer><?php echo error_footer ?></footer>
+      </section>
+   </body>
+</html>
