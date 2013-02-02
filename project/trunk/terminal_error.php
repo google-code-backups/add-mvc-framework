8fd5da6be227bcf527d77ec1d6d93449bf709f5a
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-02 00:16:07 +0000

    Fix Issue: http://code.google.com/p/add-mvc-framework/issues/detail?id=24
    Terminal Error Page

--- project/trunk/terminal_error.php
+++ project/trunk/terminal_error.php
@@ -6,6 +6,17 @@ $error_message = error_message;
 
 $error_message = preg_replace('/(?<= )(\S{30,})(?= )/','<br />\1<br />',$error_message);
 
+$backtraces = error_trace;
+$file_lines = array();
+$dirname = dirname(__FILE__);
+foreach (explode("\n",$backtraces) as $backtrace) {
+   $backtrace = preg_replace('/^\#\d+\s+/',"",$backtrace);
+   $backtrace = str_replace($dirname,".",$backtrace);
+   $backtrace = str_replace(": ","\r\n   ",$backtrace);
+   $backtrace = str_replace("::","\r\n   ::",$backtrace);
+   $backtrace = str_replace("->","\r\n   ->",$backtrace);
+   $file_lines[] = $backtrace;
+}
 ?>
 <html>
    <head>
@@ -20,7 +31,7 @@ $error_message = preg_replace('/(?<= )(\S{30,})(?= )/','<br />\1<br />',$error_m
             background:#fff;
             border-radius:5px;
             margin:20% auto;
-            width:600px;
+            width:700px;
             box-shadow:0 0 6px 3px #f33;
          }
          header {
@@ -31,20 +42,27 @@ $error_message = preg_replace('/(?<= )(\S{30,})(?= )/','<br />\1<br />',$error_m
             font-weight:bold;
             border-radius:5px 5px 0 0;
          }
+         article {
+            color:#333;
+            background:#fff;
+         }
          h1 {
             padding:10px 20px;
             font-size:10px;
             font-weight:bold;
             text-align:center;
             line-height:20px;
+            color:#a00;
          }
          p {
             font:8px verdana;
             text-align:center;
          }
          textarea {
-            width:100%;
+            width:90%;
             height:300px;
+            margin:0 auto;
+            display:block;
          }
          footer {
             padding:5px 10px;
@@ -61,6 +79,11 @@ $error_message = preg_replace('/(?<= )(\S{30,})(?= )/','<br />\1<br />',$error_m
          <header><?php echo error_header ?></header>
          <article>
             <h1><?php echo $error_message ?></h1>
+            <ul>
+               <?php foreach ($file_lines as $file_line): ?>
+               <li><xmp><?php echo $file_line ?></xmp></li>
+               <?php endforeach ?>
+            </ul>
             <p>Contact the system administrator with the error printed above</p>
          </article>
          <footer><?php echo error_footer ?></footer>
