e6911a91808b40e65656beb0a2ec1bd1b2b9c261
brian.requinala@247talk.net <brian.requinala@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-07 17:26:00 +0000

    fix bug:http://code.google.com/p/add-mvc-framework/issues/detail?id=32

--- project/trunk/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
+++ project/trunk/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
@@ -13,9 +13,13 @@ CLASS Smarty_Internal_Compile_Add_Layout EXTENDS Smarty_Internal_Compile_Extends
     * @since ADD MVC 0.10
     */
    public function getAttributes() {
-      $result = call_user_func_array('parent::'.__FUNCTION__,func_get_args());
-      $result['file'] = preg_replace('/^(\\\'|\")/','$0layouts/',$result['file']);
-      $result['file'] = preg_replace('/(\\\'|\")$/','.tpl$0',$result['file']);
+      $args = func_get_args();
+      if (isset($args[1][0]['file'])) {
+         $args[1][0]['file'] = preg_replace('/^(\\\'|\")/','$0layouts/',$args[1][0]['file']);
+         $args[1][0]['file'] = preg_replace('/(\\\'|\")$/','.tpl$0',$args[1][0]['file']);
+      }
+      $result = call_user_func_array('parent::'.__FUNCTION__,$args);
+
       return $result;
    }
 }
