0aab49f52ff6ef06e218922615cce7a4b1929ff1
jezieltabora@247talk.net <jezieltabora@247talk.net@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-08 19:20:14 +0000

    Issue 24 in add-mvc-framework: SmartyException is not handled gracefully

--- project/trunk/classes/exceptions/e_smarty.class.php
+++ project/trunk/classes/exceptions/e_smarty.class.php
@@ -5,4 +5,13 @@
  */
 CLASS e_smarty EXTENDS e_system {
 
+   /**
+    * Handle exception
+    *
+    * @since
+    */
+   public function handle_exception() {
+      $error_message = preg_replace('/(https?:|(<)add_mvc_path(>)|www|(.):)(.+)(tpl|php)(.+)([0-9]+)/', add::config()->add_dir, $this->data->message);
+      $this->data->message = $error_message;
+   }
 }
