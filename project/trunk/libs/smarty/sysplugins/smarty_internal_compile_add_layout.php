b9953fb7b2fdceb4fe31341f1449580edbf71e46
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-12 16:24:11 +0000

    Resolve Issue: http://code.google.com/p/add-mvc-framework/issues/detail?id=40

--- project/trunk/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
+++ project/trunk/libs/smarty/sysplugins/smarty_internal_compile_add_layout.php
@@ -10,9 +10,14 @@ CLASS Smarty_Internal_Compile_Add_Layout EXTENDS Smarty_Internal_Compile_Extends
    /**
     * Here we change the path to layouts/path
     *
+    * @param object $compiler
+    * @param array $attributes
+    *
+    * @see Smarty_Internal_CompileBase::getAttributes($compiler, $attributes)
+    *
     * @since ADD MVC 0.10
     */
-   public function getAttributes() {
+   public function getAttributes($compiler, $attributes) {
       $args = func_get_args();
       if (isset($args[1][0]['file'])) {
          $args[1][0]['file'] = preg_replace('/^(\\\'|\")/','$0layouts/',$args[1][0]['file']);
