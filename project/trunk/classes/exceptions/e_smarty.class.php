2f8a99084b6bb99d57f20e7cadb41d5fe36a2ea2
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-02-12 18:03:09 +0000

    Resolve Issue: http://code.google.com/p/add-mvc-framework/issues/detail?id=24

--- project/trunk/classes/exceptions/e_smarty.class.php
+++ project/trunk/classes/exceptions/e_smarty.class.php
@@ -10,8 +10,9 @@ CLASS e_smarty EXTENDS e_system {
     *
     * @since
     */
-   public function handle_exception() {
-      $error_message = preg_replace('/((&quot;)https?:|(<)add_mvc_path(>)|www)(.+)(tpl|php)(.+)([0-9]+)/', '&quot;'.add::config()->add_dir.'&quot;', $this->data->message);
-      $this->data->message = $error_message;
+   public function getMessage() {
+      $error_message = parent::getMessage();
+      $error_message = str_replace(add::config()->root_dir, 'add-mvc-path', $error_message);
+      return $error_message;
    }
 }
