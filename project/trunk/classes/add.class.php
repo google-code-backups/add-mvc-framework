da7f3bfb8deb0d1730aca5b7bc79ad8e15f19ddd
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-03-02 00:03:25 +0000

    bug fix: 	When an exception is thrown when handling exception, only the exception thrown and not the original one https://code.google.com/p/add-mvc-framework/issues/detail?id=53

--- project/trunk/classes/add.class.php
+++ project/trunk/classes/add.class.php
@@ -256,7 +256,7 @@ CLASS add {
       catch (Exception $e2) {
          echo "<div style='color:red'>".get_class($e)."(#".$e->getCode().")".$e->getMessage()."\r\n".$e->getFile().":".$e->getLine()."</div>";
          if ($e2 != $e) {
-            echo "<div style='color:red'>".get_class($e)."(#".$e->getCode().")".$e->getMessage()."\r\n".$e->getFile().":".$e->getLine()."</div>";
+            echo "<div style='color:red'>".get_class($e2)."(#".$e2->getCode().")".$e2->getMessage()."\r\n".$e2->getFile().":".$e2->getLine()."</div>";
          }
          die();
       }
