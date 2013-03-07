66997d3b6d1c12c804d0abcfeb702dfe99b8416c
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-03-07 20:32:19 +0000

    Tag 0.10.5

---
+++ project/tags/0.10.5/classes/smarty/e_add_smarty.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Smarty view for exceptions
+ *
+ * @author albertdiones@gmail.com
+ *
+ * @package ADD MVC\Extras
+ * @since ADD MVC 0.7
+ * @version 0.0
+ *
+ * @todo make the config variables changeable outside framework
+ */
+CLASS e_add_smarty EXTENDS add_smarty {
+
+   /**
+    * Smarty construct
+    *
+    * @since ADD MVC 0.7
+    */
+   public function __construct() {
+
+      parent::__construct();
+
+      $this -> setTemplateDir(
+            array(
+                  add::config()->views_dir.'/exceptions',
+                  add::config()->add_dir.'/views/exceptions'
+               )
+         );
+
+   }
+
+}
