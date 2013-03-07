665ae6ad48f278f22b4d291e7766ef50b64f0622
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-03-07 20:32:19 +0000

    Tag 0.10.5

---
+++ project/tags/0.10.5/classes/debug/debug.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Debugger class
+ * Override this on application for customized debugging
+ *
+ * @author albertdiones@gmail.com
+ *
+ * @package ADD MVC\Debuggers
+ * @since ADD MVC 0.0
+ * @version 0.1
+ */
+ABSTRACT CLASS debug EXTENDS add_debug {
+
+}
