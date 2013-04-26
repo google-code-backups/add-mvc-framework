00c19d7f339d00f412d723e73e6d2902ccf4e654
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-04-26 19:02:11 +0000

    build_url function is not compatible with JavaScript hrefs
    https://code.google.com/p/add-mvc-framework/issues/detail?id=81
    
    if the url starts with "javascript:" then neglect the base url

--- project/trunk/functions/url.functions.php
+++ project/trunk/functions/url.functions.php
@@ -3,39 +3,49 @@
 /**
  * Returns the complete url according to $base
  *
+ * @param string $base
+ * @param string $url
+ *
  * @since ADD MVC 0.5
  */
 function build_url($base,$url) {
-  $base_parts=url_parts($base);
+   $base_parts=url_parts($base);
 
-  if ($url[0]==='/') {
-    return rtrim($base_parts['protocol_domain'],'/').$url;
-  }
-  if ($url[0]==='?') {
-    if (!$base_parts['pathname'])
-      $base_parts['pathname']='/';
-    return $base_parts['protocol_domain'].$base_parts['pathname'].$url;
-  }
-  if ($url[0]==='#') {
+   # https://code.google.com/p/add-mvc-framework/issues/detail?id=81
+   if (preg_match('/^javascript\:/',$url)) {
+      return $url;
+   }
 
-  }
-  if (preg_match('/^https?\:\/+/',$url)) {
-    return $url;
-  }
+   if ($url[0]==='/') {
+      return rtrim($base_parts['protocol_domain'],'/').$url;
+   }
+   if ($url[0]==='?') {
+      if (!$base_parts['pathname'])
+         $base_parts['pathname']='/';
+      return $base_parts['protocol_domain'].$base_parts['pathname'].$url;
+   }
+   if ($url[0]==='#') {
 
-  return rtrim($base_parts['protocol_domain'],"/").$base_parts['path'].$url;
+   }
+   if (preg_match('/^https?\:\/+/',$url)) {
+      return $url;
+   }
+
+   return rtrim($base_parts['protocol_domain'],"/").$base_parts['path'].$url;
 }
 
 /**
  * Returns the URL parts of the url
  *
+ * @param string $Url
+ *
  * @since ADD MVC 0.5
  */
 function url_parts($url) {
-  if (!preg_match('/^(?P<protocol_domain>(?P<protocol>https?\:\/+)(?P<domain>([^\/\W]|[\.\-])+))(?P<request_uri>(?P<pathname>(?P<path>\/(.+\/)?)?(?P<file>[^\?\#]+?)?)?(?P<query_string>\?[^\#]*)?)(\#(?P<hash>.*))?$/',$url,$url_parts)) {
-    echo debug_backtrace();
-    throw new Exception("Invalid url: $url");
-  }
-  return $url_parts;
+   if (!preg_match('/^(?P<protocol_domain>(?P<protocol>https?\:\/+)(?P<domain>([^\/\W]|[\.\-])+))(?P<request_uri>(?P<pathname>(?P<path>\/(.+\/)?)?(?P<file>[^\?\#]+?)?)?(?P<query_string>\?[^\#]*)?)(\#(?P<hash>.*))?$/',$url,$url_parts)) {
+      echo debug_backtrace();
+      throw new Exception("Invalid url: $url");
+   }
+   return $url_parts;
 }
 ?>
