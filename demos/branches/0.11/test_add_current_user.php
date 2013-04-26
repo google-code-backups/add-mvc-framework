b7b343c8b65cf02e589691dfdc016052d6d02cdf
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-04-26 18:32:21 +0000

    test add current user

---
+++ demos/branches/0.11/test_add_current_user.php
@@ -0,0 +1,28 @@
+<?php
+require '../config.php';
+require $C->add_dir.'/init.php';
+
+/**
+ * Blank concrete class for overloading
+ *
+ */
+CLASS current_user EXTENDS add_current_user {
+   static $do_track = array(
+         self::TRACK_BROWSER,
+         self::TRACK_REFERER,
+         self::TRACK_IP
+      );
+}
+
+debug::print_eval('current_user::ip_in_network()');
+debug::print_eval('current_user::is_developer()');
+debug::print_eval('current_user::track()');
+#debug::print_eval('current_user::v()');
+
+if ($_REQUEST['mode'] == 'reset') {
+   session_destroy();
+}
+
+?>
+<a href="?rand=<?php echo rand() ?>">Test Referer</a>
+<a href="?mode=reset">Reset session</a>
