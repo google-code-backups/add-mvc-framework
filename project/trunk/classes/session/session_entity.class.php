2dd83e7247268c0a1c4af3d0a7ab8d8cf251d365
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-04-30 19:32:52 +0000

    array_entity class::singleton() will have more than 1 instance
    https://code.google.com/p/add-mvc-framework/issues/detail?id=83
    
    bug fix: __construct of array_entity since it is faulty

--- project/trunk/classes/session/session_entity.class.php
+++ project/trunk/classes/session/session_entity.class.php
@@ -18,9 +18,14 @@ CLASS session_entity EXTENDS array_entity {
     */
    public $session_var;
 
-
+   /**
+    * Do not inherit the __construct of array_entity since it is faulty ( see issue #83 )
+    *
+    * @see https://code.google.com/p/add-mvc-framework/issues/detail?id=83
+    *
+    */
    protected function __construct(&$session_var) {
-      parent::__construct($session_var);
+      #parent::__construct($session_var);
       $this->data = &$session_var;
       $this->session_var = &$this->data;
    }
