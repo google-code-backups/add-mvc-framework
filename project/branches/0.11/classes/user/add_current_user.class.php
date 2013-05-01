b21916069f7fa63721bd364f5fa5a0911e2e33c5
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-05-01 19:27:03 +0000

    [0.11] "add_current_user" & "current_user"
    https://code.google.com/p/add-mvc-framework/issues/detail?id=80
    
    tracking diff style

--- project/branches/0.11/classes/user/add_current_user.class.php
+++ project/branches/0.11/classes/user/add_current_user.class.php
@@ -19,8 +19,18 @@ ABSTRACT CLASS add_current_user EXTENDS session_entity IMPLEMENTS i_singleton {
    const TRACK_BROWSER = 1;
    const TRACK_REFERER = 2;
    const TRACK_IP      = 4;
-   const TRACK_GPC     = 8;
+   const TRACK_REQUEST = 8;
    const TRACK_SESSION = 16;
+   const TRACK_SERVER  = 32;
+
+
+   /**
+    * Server variables indexes to track (aside from http_*)
+    *
+    */
+   protected static $_SERVER_indexes = array(
+         'REQUEST_URI','REMOTE_ADDR'
+      );
 
    /**
     * Whether to track pages or not
@@ -179,6 +189,10 @@ ABSTRACT CLASS add_current_user EXTENDS session_entity IMPLEMENTS i_singleton {
             'request_time' => $_SERVER['REQUEST_TIME']
          );
 
+      if ($activities) {
+         $last_activity = array_pop($activities);
+      }
+
       if (
             in_array(static::TRACK_REFERER,static::$do_track)
             &&
@@ -188,18 +202,13 @@ ABSTRACT CLASS add_current_user EXTENDS session_entity IMPLEMENTS i_singleton {
 
          $is_referer_recorded = false;
 
-         if ($activities) {
-            $last_activity = array_pop($activities);
+         if ($last_activity) {
             if (!empty($last_activity['record_time'])) {
                $is_referer_recorded = $last_activity['url'] == $track_data['referer'];
                if ($is_referer_recorded) {
                   $last_activity['time_on_page'] = $track_data['request_time'] - $last_activity['record_time'];
                }
             }
-            array_push(
-                  $activities,
-                  $last_activity
-               );
          }
          # Record the referer data
          if (!$is_referer_recorded) {
@@ -213,33 +222,77 @@ ABSTRACT CLASS add_current_user EXTENDS session_entity IMPLEMENTS i_singleton {
          }
       }
 
+      $track_data2 = array();
+
       if (in_array(static::TRACK_BROWSER,static::$do_track)) {
-         $track_data['browser'] = $_SERVER['HTTP_USER_AGENT'];
+         $track_data2['browser'] = $_SERVER['HTTP_USER_AGENT'];
       }
 
       if (in_array(static::TRACK_IP,static::$do_track)) {
-         $track_data['ip'] = $_SERVER['REMOTE_ADDR'];
+         $track_data2['ip'] = $_SERVER['REMOTE_ADDR'];
       }
 
-      $track_data['superglobals'] = array();
-
-      if (in_array(static::TRACK_GPC,static::$do_track)) {
-         $track_data['superglobals']['_GET']    = $_GET;
-         $track_data['superglobals']['_POST']   = $_POST;
-         $track_data['superglobals']['_COOKIE'] = $_COOKIE;
+      if (in_array(static::TRACK_REQUEST,static::$do_track)) {
+         $track_data2['_POST']    = $_POST;
+         $track_data2['_COOKIE']  = $_COOKIE;
+         $track_data2['_REQUEST'] = $_REQUEST;
       }
 
       if (in_array(static::TRACK_SESSION,static::$do_track)) {
-         $track_data['superglobals']['_SESSION'] = $_SESSION;
+         $track_data2['_SESSION'] = $_SESSION;
+      }
+
+      if (in_array(static::TRACK_SERVER,static::$do_track)) {
+         $track_data2['_SERVER'] = array();
+         foreach ($_SERVER as $_SERVER_index => $_SERVER_value) {
+            if ( strpos($_SERVER_index,'HTTP_') === 0 || in_array($_SERVER_index, static::$_SERVER_indexes) ) {
+               $track_data2['_SERVER'][$_SERVER_index] = $_SERVER_value;
+            }
+         }
+      }
+
+      if ($last_activity) {
+         foreach ($track_data2 as $track_data2_index => &$track_data2_value) {
+            $track_data2_value = array_diff($track_data2_value, $last_activity[$track_data2_index]);
+         }
+         array_push(
+               $activities,
+               $last_activity
+            );
       }
 
       array_push(
             $activities,
-            $track_data
+            array_merge($track_data,$track_data2)
          );
 
       $singleton->activities = $activities;
 
+   }
+
+
+   /**
+    * Track stay records the stay on page of a user (good for ajax-ping)
+    *
+    */
+   public function track_stay() {
+      $singleton = isset($this) ? $this : static::singleton();
+
+      $activities = $singleton->activities;
+
+      if (!isset($activities)) {
+         $activities = array();
+      }
+
+      if ($activities) {
+         $last_activity = array_pop($activities);
+         $last_activity['time_on_page'] = microtime(true) - $last_activity['request_time'];
+         array_push(
+               $activities,
+               $last_activity
+            );
+      }
+      $singleton->activities = $activities;
    }
 
 
