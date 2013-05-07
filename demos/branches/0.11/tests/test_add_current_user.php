431333980d1634f7d41f3b1b76f8a7671139affd
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-05-07 16:14:41 +0000

    [0.11] "add_current_user" & "current_user"
    https://code.google.com/p/add-mvc-framework/issues/detail?id=80
    
    test file

--- demos/branches/0.11/tests/test_add_current_user.php
+++ demos/branches/0.11/tests/test_add_current_user.php
@@ -2,28 +2,19 @@
 require '../config.php';
 require $C->add_dir.'/init.php';
 
-/**
- * Blank concrete class for overloading
- *
- */
-CLASS current_user EXTENDS add_current_user {
-   static $do_track = array(
-         self::TRACK_BROWSER,
-         self::TRACK_REFERER,
-         self::TRACK_IP,
-         self::TRACK_GPC,
-         self::TRACK_SESSION
-      );
-}
-
 if ($_REQUEST['mode'] == 'reset') {
+   session_start();
    session_destroy();
 }
 
 debug::print_eval('current_user::ip_in_network()');
 debug::print_eval('current_user::is_developer()');
-debug::print_eval('current_user::track()');
 #debug::print_eval('current_user::v()');
+
+$user = current_user::singleton();
+
+debug::var_dump($user->activities);
+
 
 ?>
 <a href="?rand=<?php echo rand() ?>">Test Referer</a>
