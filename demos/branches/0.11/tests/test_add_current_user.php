4dcd73343f76b5b19f73ae6b43f0f589a6dcb541
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-05-08 18:45:20 +0000

    [0.11] "add_current_user" & "current_user"
    https://code.google.com/p/add-mvc-framework/issues/detail?id=80
    
    Removed old debug on demo test

--- demos/branches/0.11/tests/test_add_current_user.php
+++ demos/branches/0.11/tests/test_add_current_user.php
@@ -7,10 +7,6 @@ if ($_REQUEST['mode'] == 'reset') {
    session_destroy();
 }
 
-debug::print_eval('current_user::ip_in_network()');
-debug::print_eval('current_user::is_developer()');
-#debug::print_eval('current_user::v()');
-
 $user = current_user::singleton();
 
 debug::var_dump($user->trimmed_activities());
@@ -20,6 +16,6 @@ debug::var_dump($user->trimmed_activities());
 <a href="?rand=<?php echo rand() ?>">Test Referer</a>
 <a href="?mode=reset">Reset session</a>
 <form method="post">
-   <textarea name="rand<?php echo rand() ?>"></textarea><br />
+   <textarea name="rand<?php echo rand() ?>"></textarea>
    <button type="submit">Submit</button>
 </form>
