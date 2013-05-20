e2bc978831cdf7bceb91304bbf0196e0cac8f9ae
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-05-20 20:27:11 +0000

    fix: documentation

--- project/trunk/classes/add/add_adodb.class.php
+++ project/trunk/classes/add/add_adodb.class.php
@@ -2,6 +2,11 @@
 /**
  * adodb wrapper abstract
  * A class to be used as wrapper on adodb
+ * One child class = one database connection, therefore for each database connection you must create a concrete class or use the 2 built in concrete class mysql and mssql
+ *
+ * @see add_adodb_mysql , add_adodb_mssql
+ *
+ *
  * Being considered to be deprecated
  * @package ADD MVC\ADODB wrappers
  * @version 0.0
@@ -20,10 +25,24 @@ ABSTRACT CLASS add_adodb {
     */
    public $adodb;
 
+   /**
+    * Contains data about the last operation
+    *
+    * @see __call and check_error()
+    *
+    */
    public $add_last_operation;
 
+   /**
+    * Error codes to ignore, those that are normally throwing such as insert duplicate error
+    *
+    */
    static $ignore_error_numbers = array();
 
+   /**
+    * Creates a single instance for the adodb class
+    *
+    */
    static function singleton() {
       $adodb = ADONewConnection(static::DB_TYPE);
       $global_varname = static::varname();
@@ -45,15 +64,26 @@ ABSTRACT CLASS add_adodb {
       return $GLOBALS[$global_varname];
    }
 
+   /**
+    * Construction
+    *
+    */
    function __construct($adodb) {
       $this->adodb = $adodb;
    }
 
-
+   /**
+    * Prevent to be cloned
+    *
+    */
    public function __clone() {
       throw new Exception('Clone is not allowed.');
    }
 
+   /**
+    * Record the function and the arguments then call the adodb method
+    *
+    */
    function __call($method, $args) {//call adodb methods
       $this->add_last_operation = array(array($this->adodb, $method),$args);
       $this->add_last_operation['result'] = call_user_func_array(array($this->adodb, $method),$args);
@@ -61,14 +91,26 @@ ABSTRACT CLASS add_adodb {
       return $this->add_last_operation['result'];
    }
 
+   /**
+    * Get the property of the inner adodb object
+    *
+    */
    function __get($property) {
       return $this -> adodb -> $property;
    }
 
+   /**
+    * Set the property of the inner adodb object
+    *
+    */
    function __set($property, $value) {
       $this ->adodb -> $property = $value;
    }
 
+   /**
+    * Check if there is error on the last operation
+    *
+    */
    function check_error() {
       $last_operation = &$this->add_last_operation;
       if (!$last_operation['result'] && ($last_operation['error_message'] = $this->adodb->ErrorMsg()) && !in_array($this->adodb->ErrorNo(),static::$ignore_error_numbers)) {
@@ -76,14 +118,19 @@ ABSTRACT CLASS add_adodb {
       }
    }
 
+
+   /**
+    * Handle the error, extend this to handle specific erors
+    *
+    */
    function handle_error() {
       trigger_error("ADODB Error: (".$this->adodb->ErrorNo().")".$this->add_last_operation['error_message']);
    }
 
-   static function debug() {
-      return debug::var_dump(func_get_args());
-   }
-
+   /**
+    * Extend this method to specify the credentials
+    *
+    */
    abstract public function Connect();
 
    /**
