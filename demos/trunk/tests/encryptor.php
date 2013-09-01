0d546beaa2e971e87b9f3891131632e8d6f262e9
albertdiones <albertdiones@gmail.com>
2013-09-01 13:39:21 +0000

    Encryptor tester

---
+++ demos/trunk/tests/encryptor.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * add_encryptor tset
+ * @see https://code.google.com/p/add-mvc-framework/issues/detail?id=111
+ */
+require 'add_configure.php';
+
+CLASS my_encryptor EXTENDS add_encryptor {
+   public $salt = 'foo';
+}
+
+CLASS my_encryptor2 EXTENDS add_encryptor {
+   public $salt = 'baz';
+}
+
+$string = 'bar';
+
+$encryptor = new my_encryptor($string);
+
+$encrypted = $encryptor->encrypt();
+
+debug::var_dump($encrypted);
+
+$encryptor2 = new my_encryptor2($encrypted);
+
+$encrypted2 = $encryptor2->encrypt(); 
+
+debug::var_dump($encrypted2);
+
+$encryptor3 = new my_encryptor($encrypted2,'lalala');
+
+$encrypted3 = $encryptor3->encrypt(); 
+
+debug::var_dump($encrypted3);
+
+$encryptor4 = new my_encryptor2($encrypted3,'lololo');
+
+$encrypted4 = $encryptor4->encrypt(); 
+
+debug::var_dump($encrypted4);
+
+
+$decryptor = my_encryptor2::from_encrypted($encrypted4,'lololo');
+
+$decrypted = $decryptor->decrypt();
+
+debug::var_dump();
+
+$decryptor2 = my_encryptor::from_encrypted($decrypted,'lololo');
+
+$decrypted2 = $decryptor2->decrypt();
+
+debug::var_dump($decrypted2);
+
+$decryptor3 = my_encryptor2::from_encrypted($decrypted2);
+
+$decrypted3 = $decryptor3->decrypt();
+
+debug::var_dump($decrypted3);
+
+$decryptor4 = my_encryptor::from_encrypted($decrypted3);
+
+$decrypted4 = $decryptor4->decrypt();
+
+debug::var_dump($decrypted3);
+
+
+
