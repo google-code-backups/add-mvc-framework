6d63b872858a359becbf7ea525453c37580f02a2
albertdiones <albertdiones@0b53ae53-8589-cf7e-41ad-2f2ae0fd6e93>
2013-09-05 23:15:43 +0000

    Code pointer on errors on shutdown handle is pretty useless
    https://code.google.com/p/add-mvc-framework/issues/detail?id=122
    
    deprecated error

--- project/trunk/classes/add.class.php
+++ project/trunk/classes/add.class.php
@@ -347,7 +347,7 @@ CLASS add {
             E_PARSE           => 'E_PARSE',
             E_NOTICE          => 'E_NOTICE',
             E_STRICT          => 'E_STRICT',
-            E_STRICT          => 'E_DEPRECATED',
+            E_DEPRECATED      => 'E_DEPRECATED',
             E_CORE_ERROR      => 'E_CORE_ERROR',
             E_CORE_WARNING    => 'E_CORE_WARNING',
             E_COMPILE_ERROR   => 'E_COMPILE_ERROR',
